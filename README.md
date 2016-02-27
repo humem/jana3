ce3207ca119f46749e31fe513ad86b24fb054b67
Hiroshi Umemoto <hiroshi.umemoto@gmail.com>
2016-02-27 23:59:31 +0900

    change base to jupyter/datascience-notebook

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JANA3
 
-Docker container for Japanese analysis with Anaconda3.
+Docker Container for Japanese Analysis with Python3/Jupyter.
 
 
 Usage
@@ -9,6 +9,3 @@ You can download and use this image by pulling it:
 
     docker pull humem/jana3
     docker run -i -t -v $HOME/work:/work humem/jana3
-
-
-[anaconda]: http://docs.continuum.io/anaconda/index.html
--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ targets=(
     "https://mecab.googlecode.com/files/ mecab-ipadic-2.7.0-20070801.tar.gz"
     "https://repo.continuum.io/archive/ Anaconda3-2.5.0-Linux-x86_64.sh"
     "https://github.com/dmlc/xgboost/archive/ xgboost-0.47.tar.gz 0.47.tar.gz"
-    "https://storage.googleapis.com/tensorflow/linux/cpu/ tensorflow-0.7.0-py3-none-linux_x86_64.whl"
+    "https://storage.googleapis.com/tensorflow/linux/cpu/ tensorflow-0.7.1-cp34-none-linux_x86_64.whl"
 )
 
 for target in "${targets[@]}"; do
